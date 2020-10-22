9de32bc0cb93b927fc4268aa8a2026afe50beb97
nugrazurus <nugrazurus@student.untan.ac.id>
2020-10-22 10:52:39 +0700

    first commit

---
+++ resources/views/catatan-identifikasi.blade.php
@@ -0,0 +1,34 @@
+@extends('layout')
+@section('content')
+<table class="border-collapse border-gray-500 border-2">
+    <thead>
+        <tr>
+            <th class="border-2">Pengawasan</th>
+            <th class="border-2">Identifikasi Resiko</th>
+        </tr>
+    </thead>
+    <tbody>
+        <tr>
+            <th class="border-2" rowspan="3">Pengendalian Gratifikasi</th>
+            <td class="border-2">public campaign</td>
+        </tr>
+        <tr>
+            <td class="border-2">unit pelaksana gratifikasi</td>
+        </tr>
+        <tr>
+            <td class="border-2">mengimplementasikan pengendalian gratifikasi</td>
+        </tr>
+        <tr>
+            <th class="border-2" rowspan="4">SPIP</th>
+            <td class="border-2">membangun lingkungan pengendalian</td>
+        </tr>
+        <tr>
+            <td class="border-2">telah melakukan penilaian risiko</td>
+        </tr>
+        <tr>
+            <td class="border-2">melakukan</td>
+        </tr>
+    </tbody>
+</table>
+
+@endsection
